281591ea7491b86b96da3da85e384dbaa4ab3827
Marco <mark_o002@hotmail.com>
2026-01-16 19:01:31 -0600

    feat: mongodb connection config

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"log"
+
 	"github.com/marcocesar1/Go-Service-Omnicloud/src/internal/infrastructure/http"
+	"github.com/marcocesar1/Go-Service-Omnicloud/src/internal/infrastructure/persistance/mongo_db"
 )
 
 func main() {
+	mongoConfig := mongo_db.NewMongoConfig("mongodb://root:rootpassword@localhost:27018")
+
+	if err := mongoConfig.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := mongoConfig.Disconnect(); err != nil {
+			log.Println("Error disconnecting MongoDB:", err)
+		}
+	}()
+
+	mongoConfig.CreateCollections()
+
 	server := http.NewServer()
 	server.Start()
 }
---
+++ src/internal/infrastructure/persistance/mongo_db/mongo_config.go
@@ -0,0 +1,60 @@
+package mongo_db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+type MongoConfig struct {
+	DbUrl  string
+	client *mongo.Client
+}
+
+func NewMongoConfig(dbUrl string) *MongoConfig {
+	return &MongoConfig{
+		DbUrl: dbUrl,
+	}
+}
+
+func (m *MongoConfig) Connect() error {
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(m.DbUrl).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		return err
+	}
+
+	if err := client.Database("admin").
+		RunCommand(context.TODO(), bson.D{{"ping", 1}}).
+		Err(); err != nil {
+		return err
+	}
+
+	m.client = client
+
+	fmt.Println("Successfully connected to MongoDB!")
+	return nil
+}
+
+func (m *MongoConfig) Disconnect() error {
+	if m.client == nil {
+		return nil
+	}
+	return m.client.Disconnect(context.TODO())
+}
+
+func (m *MongoConfig) CreateCollections() {
+	database := m.client.Database("admin")
+
+	err := database.CreateCollection(context.TODO(), "people")
+	if err != nil {
+		log.Fatalf("Failed to create people collection: %v", err)
+	}
+}
